bd9a324d9cb4f38624100f18fcb0ebd4757d6f68
Tushar Koul <tusharkoul@gmail.com>
2017-08-11 16:37:35 -0700

    adding an image

--- SearchPage.js
+++ SearchPage.js
@@ -26,6 +26,7 @@ export default class SearchPage extends Component {
                     title='Go'
                 />
             </View>
+            <Image source={require('./Resources/house.png')} style={styles.image}/>
         </View>
         );
     }
@@ -62,5 +63,10 @@ const styles = StyleSheet.create({
     },
     goButton: {
         color:'#48BBEC',
-    }
+    },
+    image: {
+        width: 217,
+        height: 138,
+    },
+
 });
