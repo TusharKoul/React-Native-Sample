754096ccee0cc3c003bb5d2e77bc97ca4027f8c7
Tushar Koul <tusharkoul@gmail.com>
2017-08-15 17:54:19 -0700

    adding CalendarManager native module

---
+++ ios/PropertyFinder/CalendarManager.h
@@ -0,0 +1,14 @@
+//
+//  CalendarManager.h
+//  PropertyFinder
+//
+//  Created by Tushar Koul on 8/15/17.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <React/RCTBridgeModule.h>
+
+@interface CalendarManager : NSObject <RCTBridgeModule>
+@end
---
+++ ios/PropertyFinder/CalendarManager.m
@@ -0,0 +1,19 @@
+//
+//  CalendarManager.m
+//  PropertyFinder
+//
+//  Created by Tushar Koul on 8/15/17.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#import "CalendarManager.h"
+
+@implementation CalendarManager
+
+// To export a module named CalendarManager
+RCT_EXPORT_MODULE();
+
+// This would name the module AwesomeCalendarManager instead
+// RCT_EXPORT_MODULE(AwesomeCalendarManager);
+
+@end
